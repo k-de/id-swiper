296234cfe662f1ec125566e6d541b71494495699
k-de <kybow@protonmail.com>
2019-09-16 13:44:25 -0500

    initial commit

---
+++ ID-Swiper.cpp
@@ -0,0 +1,12 @@
+/*
+class ClassName
+{
+    private:
+        // Declarations of private members
+
+    public:
+        // Declarations of public members
+}; // Always end a class definition with a semicolon!!!!
+
+*/
+
